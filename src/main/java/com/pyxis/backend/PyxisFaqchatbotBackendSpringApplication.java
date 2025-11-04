b40b1e58dd413fd143d4441c8044e66eeab2a499
jisu9169 <wltn9169@naver.com>
2025-11-04 11:05:24 +0900

    init: upload initial Spring Boot project

---
+++ src/main/java/com/pyxis/backend/PyxisFaqchatbotBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.pyxis.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyxisFaqchatbotBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyxisFaqchatbotBackendSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pyxis/backend/PyxisFaqchatbotBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pyxis.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PyxisFaqchatbotBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
