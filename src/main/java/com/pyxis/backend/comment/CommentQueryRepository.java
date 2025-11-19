115cd0d11d9270988aa18f2371090952b5c4f8b8
jisu9169 <wltn9169@naver.com>
2025-11-19 09:24:33 +0900

    feat : QueryDSL

---
+++ src/main/java/com/pyxis/backend/comment/CommentQueryRepository.java
@@ -0,0 +1,116 @@
+package com.pyxis.backend.comment;
+
+import com.pyxis.backend.comment.dto.CommentListResponse;
+import com.pyxis.backend.comment.dto.QCommentListResponse;
+import com.pyxis.backend.comment.entity.QComment;
+import com.pyxis.backend.user.entity.QUsers;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentQueryRepository implements CommentQueryRepositoryImpl {
+
+    private final JPAQueryFactory queryFactory;
+
+    /**
+     * 최상위 댓글 조회 (+ childCount 포함)
+     */
+    @Override
+    public List<CommentListResponse> getTopComments(Long postId, int page, int size) {
+
+        QComment c = QComment.comment;
+        QUsers u = QUsers.users;
+
+        return queryFactory
+                .select(new QCommentListResponse(
+                        c.id,
+                        u.id,
+                        u.nickname,
+                        c.content,
+                        c.createdAt,
+                        c.childCount,
+                        c.status
+                ))
+                .from(c)
+                .join(c.user, u)
+                .where(
+                        c.commPost.id.eq(postId)
+                                .and(c.parentComment.isNull())
+                )
+                .orderBy(c.createdAt.desc())
+                .offset((long) page * size)
+                .limit(size)
+                .fetch();
+    }
+
+
+    /**
+     * 대댓글 조회 (+ childCount 포함)
+     */
+    @Override
+    public List<CommentListResponse> getChildComments(Long parentId, int page, int size) {
+
+        QComment c = QComment.comment;
+        QUsers u = QUsers.users;
+
+        return queryFactory
+                .select(new QCommentListResponse(
+                        c.id,
+                        u.id,
+                        u.nickname,
+                        c.content,
+                        c.createdAt,
+                        c.childCount,
+                        c.status
+                ))
+                .from(c)
+                .join(c.user, u)
+                .where(c.parentComment.id.eq(parentId))
+                .orderBy(c.createdAt.asc())
+                .offset((long) page * size)
+                .limit(size)
+                .fetch();
+    }
+
+
+    /**
+     * 최상위 댓글 count
+     */
+    @Override
+    public long countTopComments(Long postId) {
+        QComment c = QComment.comment;
+
+        Long result = queryFactory
+                .select(c.count())
+                .from(c)
+                .where(
+                        c.commPost.id.eq(postId)
+                                .and(c.parentComment.isNull())
+                )
+                .fetchOne();
+
+        return result != null ? result : 0L;
+    }
+
+    /**
+     * 대댓글 count
+     */
+    @Override
+    public long countChildComments(Long parentId) {
+
+        QComment c = QComment.comment;
+
+        Long result = queryFactory
+                .select(c.count())
+                .from(c)
+                .where(c.parentComment.id.eq(parentId))
+                .fetchOne();
+
+        return result != null ? result : 0L;
+    }
+
+}
---
+++ src/main/java/com/pyxis/backend/comment/CommentQueryRepositoryImpl.java
@@ -0,0 +1,16 @@
+package com.pyxis.backend.comment;
+
+import com.pyxis.backend.comment.dto.CommentListResponse;
+
+import java.util.List;
+
+public interface  CommentQueryRepositoryImpl {
+
+    List<CommentListResponse> getTopComments(Long postId, int page, int size);
+
+    List<CommentListResponse> getChildComments(Long parentId, int page, int size);
+
+    long countTopComments(Long postId);
+
+    long countChildComments(Long parentId);
+}
